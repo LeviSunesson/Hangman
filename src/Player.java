8ae92890c659e19776a3da0523c5b2e4f8fe4895
leviSunesson <levi@sunesson.com>
2018-02-27 14:10:47 +0100

    Setting up player class

---
+++ src/Player.java
@@ -0,0 +1,19 @@
+
+public class Player {
+
+	private static int LIVES;
+	private static int POINTS;
+	
+	public static void addPoint() {
+		
+		POINTS++;
+		
+	}
+	
+	public static void setLives(int life) {
+		
+		LIVES = life;
+		
+	}
+	
+}
